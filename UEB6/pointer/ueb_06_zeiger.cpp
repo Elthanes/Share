0e179878b9385f1f25661d2c638079fa41ddfad4
Elthanes <manuel.schneider@student.uni-siegen.de>
2014-11-25 14:46:14 +0100

    Ordnen

--- UEB6/pointer/ueb_06_zeiger.cpp
+++ UEB6/pointer/ueb_06_zeiger.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
 int main(){
-	int* p_pointer = new int;
-	*p_pointer = 21;
+	int* p_pointer = new int(21);
 	std::cout <<"Inhalt Zeigervariable: " << *p_pointer << std::endl;
 	delete p_pointer;
 	p_pointer = NULL;
